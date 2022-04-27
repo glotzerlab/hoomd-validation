5357f7b44ab2a7ee092ded9bad9913f3e61f15b8
Tommy Waltmann <tomwalt@umich.edu>
2022-04-27 15:50:28 -0400

    project files which implement dummy workflow

---
+++ hoomd_validation/all_validation_tests.py
@@ -0,0 +1,18 @@
+from flow import FlowProject
+from config import all_validation_tests, test_project_dict
+
+import lj_fluid
+
+
+class AllValidationTests(FlowProject):
+    pass
+
+
+@AllValidationTests.operation
+def run(job):
+    pr = test_project_dict[job.sp.project_name].get_project(job.sp.path)
+    pr.main()
+
+
+if __name__ == "__main__":
+    AllValidationTests.get_project(all_validation_tests.root_directory()).main()
---
+++ hoomd_validation/lj_fluid.py
@@ -0,0 +1,22 @@
+from config import test_project_dict
+from project_classes import LJFluid
+
+
+@LJFluid.operation
+def create_initial_state(job):
+    print("Created Initial LJ fluid state")
+    return
+
+@LJFluid.operation
+def run_simulation(job):
+    print("Running LJ Fluid simulation")
+    pass
+
+@LJFluid.operation
+def analyze(job):
+    print("Analyzing LJ Fluid simulation")
+    pass
+
+
+if __name__ == "__main__":
+    LJFluid.get_project(test_project_dict["LJFluid"].root_directory()).main()
