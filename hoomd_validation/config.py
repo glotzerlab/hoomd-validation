819112130074dc6df50472c244d4e1246b974265
Tommy Waltmann <tomwalt@umich.edu>
2022-04-27 15:48:45 -0400

    make directories if they don't already exist

--- hoomd_validation/config.py
+++ hoomd_validation/config.py
@@ -4,22 +4,23 @@ from pathlib import Path
 
 
 # variables for subprojects
-test_project_name_list = ['HardDisks', 'LJFluid']
+test_project_name_list = ['LJFluid']
 test_project_dict = dict()
 
 # path to all the project workspace directories
 project_root = Path(__file__).parent.parent
 
 # global project which manages all other projects
-all_validation_tests = signac.get_project(
-    str(project_root / "AllValidationTests")
+all_validation_tests = signac.init_project(
+    name="AllValidationTests",
+    root=str(project_root / "AllValidationTests")
 )
 
 # project for each validation test suite
-for project_name_str in test_project_list:
+for project_name_str in test_project_name_list:
     test_project_dict[project_name_str] = \
-        getattr(project_classes, project_name_str)(
-            str(project_root / project_name_str)
+        getattr(project_classes, project_name_str).init_project(
+            name=project_name_str,
+            root=str(project_root / project_name_str)
         )
-
 
