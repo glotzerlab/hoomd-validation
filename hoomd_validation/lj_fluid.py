086ccc969492a6467374c84927b111f166214f78
Tommy Waltmann <tomwalt@umich.edu>
2022-05-02 18:05:27 -0400

    starting writing lj fluid validation test workflow

--- hoomd_validation/lj_fluid.py
+++ hoomd_validation/lj_fluid.py
@@ -3,7 +3,7 @@
 
 """Lennard Jones phase behavior validation test.
 
-This workflow is a dummy and will be fully implemented at a later date.
+
 """
 
 from config import test_project_dict
@@ -11,23 +11,125 @@ from project_classes import LJFluid
 
 
 @LJFluid.operation
+@LJFluid.post.isfile('initial_state.gsd')
 def create_initial_state(job):
     """Create initial system configuration."""
-    print("Created initial LJ fluid state")
-    return
+    import gsd.hoomd
+    import itertools
+
+    density = ???
+    num_particles = ???
+
+    box_volume = num_particles / density
+    L = box_volume**(1/3.)
+
+    N = int(np.ceil(num_particles ** (1./3.)))
+    x = np.linspace(-L / 2, L / 2, N, endpoint=False)
+    position = list(itertools.product(x, repeat=3))[:num_particles]
+
+    # create snapshot
+    snap = gsd.hoomd.Snapshot()
+    snap.particles.N = num_particles
+    snap.particles.position = position
+    snap.particles.typeid = [0] * num_particles
+    snap.particles.types = ['A']
+
+    snap.configuration.box = [L, L, L, 0, 0, 0]
+
+    with gsd.hoomd.open(job.fn("initial_state.gsd"), "wb") as traj:
+        traj.append(snap)
 
 
 @LJFluid.operation
-def run_simulation(job):
-    """Run the simulation."""
-    print("Running LJ fluid simulation")
+@LJFluid.pre.isfile('initial_state.gsd')
+@LJFluid.pre.after(create_initial_state)
+@LJFluid.post.isfile('nvt_sim.gsd')
+def run_nvt_simulation(job):
+    """Run the simulation in NVT."""
+    import hoomd
+    from hoomd import md
+
+    epsilon = ???
+    sigma = ???
+    r_cut_coeff = ???
+    shift_mode = ???
+    seed = ???
+    nlist_params = ???
+    kT = ???
+    tau = ???
+    run_steps = ???
+    r_cut = sigma * r_cut_coeff
+
+    device = hoomd.device.CPU()
+    sim = hoomd.Simulation(device)
+    sim.create_state_from_gsd(job.fn('initial_state.gsd'))
+    sim.seed = seed
+
+    # pair force
+    nlist = md.nlist.Cell(**nlist_params)
+    lj = md.pair.LJ(default_r_cut=r_cut, nlist=nlist)
+    lj.params[('A', 'A')] = dict(sigma=sigma, epsilon=epsilon)
+    lj.shift_mode = shift_mode
+
+    # integration method
+    nvt = md.methods.NVT(kT=kT, tau=tau)
+
+    # integrator
+    integrator = md.Integrator(dt=delta_t, methods=[nvt], forces=[lj])
+    sim.operations.integrator = integrator
+
+    # compute pressure
+    thermo = md.thermo.ThermodynamicQuantities()
+    sim.operations.add(thermo)
+
+    # log the pressure
+    logger = hoomd.logging.Logger()
+    logger.add(thermo, quantities=['pressure'])
+
+    # write data to gsd file
+    gsd_writer = hoomd.write.GSD(filename=job.fn('nvt_sim.gsd'),
+                                 trigger=hoomd.trigger.Periodic(1000),
+                                 mode='wb',
+                                 log=logger)
+    sim.operations.add(gsd_writer)
+
+    # thermoalize momenta
+    sim.state.thermalize_particle_momenta(hoomd.filter.All(), kT)
+
+    # run
+    sim.run(run_steps)
+
+
+@LJFluid.operation
+@LJFluid.pre.isfile('nvt_sim.gsd')
+@LJFluid.pre.after(run_nvt_simulation)
+@LJFluid.post(lambda job: job.doc.nvt_pressure != 0.0)
+def compute_nvt_pressure(job):
+    """Compute the pressure for use in NPT simulations to cross-validate."""
     pass
 
 
 @LJFluid.operation
-def analyze(job):
-    """Analyze the output."""
-    print("Analyzing LJ fluid simulation")
+@LJFluid.pre.isfile('initial_state.gsd')
+@LJFluid.pre.after(create_initial_state)
+@LJFluid.post.isfile('npt_sim.gsd')
+def run_npt_simulation(job):
+    """Run an npt simulation at the pressure computed by the NVT simulation."""
+    pass
+
+
+@LJFluid.operation
+@LJFluid.pre.isfile('npt_sim.gsd')
+@LJFluid.pre.after(run_npt_simulation)
+@LJFluid.post(lambda job: job.doc.npt_density != 0.0)
+def compute_npt_density(job):
+    """Compute the density to cross-validate with earlier NVT simulations."""
+    pass
+
+
+def run_mc_simulation(job):
+    """There was talk of running MC simulations to further cross-validate the
+    NVT/PT results. Not sure if its really necessary at this point."""
     pass
 
 
