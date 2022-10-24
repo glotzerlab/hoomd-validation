0d27af202075f83b26e75b14b5a4842ddb1e6964
Joshua A. Anderson <joaander@umich.edu>
2022-10-24 09:50:16 -0400

    Rename project_class to project.

--- hoomd_validation/init.py
+++ hoomd_validation/init.py
@@ -6,23 +6,23 @@
 from config import all_validation_tests, test_project_dict
 
 # initialize jobs for the manager project
-for project_name_str, project_class in test_project_dict.items():
+for project_name_str, project in test_project_dict.items():
     manager_project_job = all_validation_tests.open_job(
-        dict(project_name=project_name_str, path=project_class.path))
+        dict(project_name=project_name_str, path=project.path))
     if manager_project_job not in all_validation_tests:
         manager_project_job.init()
 
 # initialize jobs for validation test projects
-for _, project_class in test_project_dict.items():
+for _, project in test_project_dict.items():
 
     # add all the jobs to the project
-    job_sps = project_class.job_statepoints
+    job_sps = project.job_statepoints
     for job_sp in job_sps:
-        job = project_class.open_job(job_sp)
-        if job not in project_class:
+        job = project.open_job(job_sp)
+        if job not in project:
             job.init()
 
         # initialize job document parameters for this job
-        for param, default in project_class.job_document_params(job):
+        for param, default in project.job_document_params(job):
             if param not in job.doc:
                 setattr(job.doc, param, default)
