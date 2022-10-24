bbc220c7010dc5da8b0a498d14fed50cf4755e5d
Joshua A. Anderson <joaander@umich.edu>
2022-10-24 13:31:43 -0400

    Fix docstring.

--- hoomd_validation/project.py
+++ hoomd_validation/project.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2022 The Regents of the University of Michigan.
 # Part of HOOMD-blue, released under the BSD 3-Clause License.
 
-"""Signac workflow for the manager project."""
+"""Project workflow entry point."""
 
 import config
 from project_class import Project
