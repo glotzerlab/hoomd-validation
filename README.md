08ea408cd7f80cf001b02e91dfc2efaec551b59c
Joshua A. Anderson <joaander@umich.edu>
2022-12-08 15:13:53 -0500

    Convert config to YAML.
    Add options.

--- README.md
+++ README.md
@@ -33,20 +33,13 @@ $ cd hoomd-validation
 ## Configuration
 
 Install the prerequisites into a Python environment of your choice. To use the
-[glotzerlab-software container], create the file `hoomd_validation/config.json`
-with the following contents:
-```
-{
-    "executable": {
-        "container_path": "<path-to>/software.sif"
-    }
-}
-```
-and replace `<path-to>` with the absolute path to the directory containing
-`software.sif`. Add any options before the path, such as
-`--bind /scratch,/gpfs <path-to>/software.sif`.
+[glotzerlab-software container], copy `hoomd_validation/config-sample.yaml` to
+`hoomd_validation/config.yaml`, uncomment the executable mapping, and set
+`singularity_container` to your container image's path.
 
-See `config_parser.py` for full documentation on all options in the config file.
+`hoomd_validation/config.yaml` also controls a number of job submission
+parameters. See the commented options in `hoomd_validation/config-sample.yaml`
+for a list and their default values.
 
 ## Usage
 
--- hoomd_validation/config_parser.py
+++ hoomd_validation/config_parser.py
@@ -6,65 +6,53 @@
 import os
 import sys
 import json
+import yaml
 from pathlib import Path
 
 
 class ConfigFile(dict):
     """Parse config files.
 
-    This class is used to parse files named config.json, which have settings
-    for configuring the execution of the validation workflows. At the moment,
-    the only configuration needed is for the executable command used by flow
-    to give as a directive for job operations. An example config file is shown
-    below:
+    Parse workflow configuration options from ``config.yaml``. See
+    ``config-sample.yaml`` for documentation of all options and their defaults.
 
-    .. code-block:: json
-
-        {
-            "executable": {
-                "container_path": "/path/to/my/container",
-                "python_exec": "/usr/bin/python"
-            }
-        }
-
-    Neither field in executable is strictly required, but if working in a
-    containerized environment, container_path must specify the location of the
-    container. If python_exec is not specified, the default behavior is to use
-    the executable being used to run the code. In a containerized environment,
-    the default behavior is to use "python", since we cannot know a priori the
-    name of the executable in the container.
-
-    The config file must be located in the hoomd_validation directory.
+    The parsed config file is presented as a dictionary in a ConfigFile
+    instance.
     """
 
-    DEFAULT_CONFIG_PATH = str(Path(__file__).parent / "config.json")
+    DEFAULT_CONFIG_PATH = str(Path(__file__).parent / "config.yaml")
 
     def __init__(self, config_file_path=DEFAULT_CONFIG_PATH):
-        self._config_path = config_file_path
-        self["executable"] = self._parse_executable_string()
+        if not os.path.exists(config_file_path):
+            config = dict()
+        else:
+            with open(config_file_path, 'r') as file:
+                config = yaml.safe_load(file)
 
-    def _parse_executable_string(self):
+        self["executable"] = self._parse_executable_string(config)
+        self["max_cores_sim"] = config.get("max_cores_sim", 16)
+        self["max_cores_submission"] = config.get("max_cores_submission", 16)
+        self["max_gpus_submission"] = config.get("max_gpus_submission", 1)
+        self["max_walltime"] = config.get("max_walltime", 24)
+
+    @staticmethod
+    def _parse_executable_string(config_file):
         """Search the config file and determine the executable.
 
         Searches the executable section of the config file and builds the string
         needed by flow's directives. If no config file is present, we use the
         python executable used to run this code.
         """
-        if not os.path.exists(self._config_path):
-            return sys.executable
-
-        return_string = ""
-        with open(self._config_path) as f:
-            config_file = json.load(f)
-
         if "executable" not in config_file:
             return sys.executable
 
+        return_string = ""
         executable_options = config_file["executable"]
-        using_container = "container_path" in executable_options
+        using_container = "singularity_container" in executable_options
         if using_container:
-            return_string += "singularity exec --nv "
-            return_string += (executable_options["container_path"] + " ")
+            return_string += "singularity exec --nv " + executable_options.get(
+                "singularity_options", "") + " "
+            return_string += (executable_options["singularity_container"] + " ")
 
         return_string += executable_options.get(
             "python_exec", "python" if using_container else sys.executable)
