d970242f38e2a986d1d11ea765fdda4a6659320d
Joshua A. Anderson <joaander@umich.edu>
2023-01-13 10:25:55 -0500

    Update copyright to 2023.

--- hoomd_validation/config.py
+++ hoomd_validation/config.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022 The Regents of the University of Michigan.
+# Copyright (c) 2022-2023 The Regents of the University of Michigan.
 # Part of HOOMD-blue, released under the BSD 3-Clause License.
 
 """Initialize signac projects and create global variables."""
