e6dffa5a45c39f3230ad93b0f1ca1cfedcbabe95
Tim Moore <mtimc@umich.edu>
2023-09-13 14:17:10 -0400

    Uncommented subprojects

--- hoomd_validation/init.py
+++ hoomd_validation/init.py
@@ -16,11 +16,11 @@ import hard_sphere
 import simple_polygon
 
 subprojects = [
-    #alj_2d,
-    #lj_fluid,
-    #lj_union,
-    #hard_disk,
-    #hard_sphere,
+    alj_2d,
+    lj_fluid,
+    lj_union,
+    hard_disk,
+    hard_sphere,
     simple_polygon,
 ]
 
